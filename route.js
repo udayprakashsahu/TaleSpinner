d1c4ab70c3a7cf525bce1063c6f5ab1787f3c0da
Uday Prakash Sahu <115711567+udayprakashsahu@users.noreply.github.com>
2024-04-25 21:40:32 +0530

    Add files via upload

---
+++ route.js
@@ -0,0 +1,29 @@
+
+function goToStory1() {
+    window.location = "story1.html";
+    return true;
+}
+
+function goToStory2() {
+    window.location = "story2.html";
+    return true;
+}
+
+function goToStory3() {
+    window.location = "story3.html";
+    return true;
+}
+
+function goToStory4() {
+    window.location = "story4.html";
+    return true;
+}
+
+function goToStory5() {
+    window.location = "story5.html";
+    return true;
+}
+function goToEnd() {
+    window.location = "end.html";
+    return true;
+}
